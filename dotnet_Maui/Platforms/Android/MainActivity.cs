0ef543cf35de1a2bbce052f7160c37891a29c143
raphi <raphi@PCHOME>
2023-03-12 17:49:02 +0100

    Test

--- dotnet_Maui/Platforms/Android/MainActivity.cs
+++ dotnet_Maui/Platforms/Android/MainActivity.cs
@@ -12,17 +12,5 @@ namespace dotnet_Maui;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Bundle savedInstanceState)
-    {
-        base.OnCreate(savedInstanceState);
-
-        ServicePointManager.ServerCertificateValidationCallback += OnServerCertificateValidationCallback;
-        var client = new HttpClient();
-    }
-
-    private bool OnServerCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-    {
-        // Trust any certificate
-        return true;
-    }
+  
 }
